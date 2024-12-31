7f1065007f6197e0e73709bdd4ecefca2cc52c0f
Sayabata Karki <sabyatakarki05@gmail.com>
2024-12-31 21:34:01 +0545

    login

--- app/src/main/java/com/example/aura_app/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/aura_app/ui/activity/LoginActivity.kt
@@ -11,7 +11,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login2)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
