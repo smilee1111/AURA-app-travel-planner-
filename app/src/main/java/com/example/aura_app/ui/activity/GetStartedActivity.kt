6f93bd8125f50f5f5213227b1a3f7295ca5da7a0
Sayabata Karki <sabyatakarki05@gmail.com>
2025-01-03 18:06:32 +0545

    login

--- app/src/main/java/com/example/aura_app/ui/activity/GetStartedActivity.kt
+++ app/src/main/java/com/example/aura_app/ui/activity/GetStartedActivity.kt
@@ -6,12 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.aura_app.R
+import com.example.aura_app.databinding.ActivityGetStartedBinding
 
 class GetStartedActivity : AppCompatActivity() {
+    lateinit var binding: ActivityGetStartedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_get_started)
+        binding=ActivityGetStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/aura_app/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/aura_app/ui/activity/LoginActivity.kt
@@ -1,21 +1,48 @@
 package com.example.aura_app.ui.activity
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.aura_app.R
+import com.example.aura_app.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        sharedPreferences = getSharedPreferences("user", Context.MODE_PRIVATE)
+
+        binding.LogIn.setOnClickListener {
+            val username: String = binding.user.text.toString()
+            val password: String = binding.Pass123.text.toString()
+
+            if (username.isEmpty()) {
+                binding.user.error = "Please enter your Username"
+            } else if (password.isEmpty()) {
+                binding.Pass123.error = "Please enter your Password"
+            } else {
+                if (binding.checkBox.isChecked) {
+                    val editor = sharedPreferences.edit()
+                    editor.putString("username", username)
+                    editor.putString("password", password)
+                    editor.apply()
+                }
+            }
+            setContentView(R.layout.activity_login)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
