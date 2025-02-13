2e13cfa0bef57154ea9007c97be39e1c8eeb510d
Sayabata Karki <sabyatakarki05@gmail.com>
2025-02-13 14:43:29 +0545

    fixing images

--- app/src/main/java/com/example/aura_app/ui/fragment/ExploreFragment.kt
+++ app/src/main/java/com/example/aura_app/ui/fragment/ExploreFragment.kt
@@ -15,15 +15,19 @@ class ExploreFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
 
     // Sample data
-    private val imageList = arrayListOf(R.drawable.ktm, R.drawable.boudha, R.drawable.himal)
-    private val nameList = arrayListOf("Nepal, Kathmandu", "Switzerland, Bern", "Other Destination")
+    private val imageList = arrayListOf(R.drawable.ktm, R.drawable.swiss, R.drawable.thailand,R.drawable.italy,
+        R.drawable.india,R.drawable.france,R.drawable.usa,)
+    private val nameList = arrayListOf("Nepal, Kathmandu", "Switzerland, Bern", "Thailand,Bangkok","Italy,Rome","India,NewDelhi","France,Paris","USA,washington DC")
     private val descList = arrayListOf(
         "Nepal, a landlocked gem nestled in South Asia, is celebrated for its breathtaking mountains, lush green terrains, and rich cultural heritage. This small yet diverse country is a haven for nature lovers, adventurers, and spiritual seekers alike.",
         "Switzerland, known for its picturesque landscapes, is a country in Europe that is famous for its mountains, lakes and  villages.",
-        "Other Destination, a place of wonder and beauty, offers unique experiences and adventures."
+        "Thailand known for tropical beaches,royal palaces, ancient temples displaying figures of Buddha. ",
+        "Italy, a European country with a long Mediterranean coastline, has left a powerful mark on Western culture and cuisine. Its capital, Rome, is home to the Vatican as well as landmark art and ancient ruins.",
+        "France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower.",
+        "The U.S. is a country of 50 states covering a vast swath of North America, with Alaska in the northwest and Hawaii extending the nation’s presence into the Pacific Ocean. Major Atlantic Coast cities are New York, a global finance and culture center, and capital Washington, DC"
     )
-    private val ratingList = arrayListOf("4.9R", "4.7R", "4.5R")
-    private val costList = arrayListOf("$500", "$1000", " $750")
+    private val ratingList = arrayListOf("4.9R", "4.7R", "4.5R","4.7R","4.2R","4.9R","4.9R")
+    private val costList = arrayListOf("$500", "$1000", " $750", "$950","$550","$1000","$1500")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
