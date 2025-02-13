59c558e7b553e04f3dbfce48a3b75340e9f9e483
Sayabata Karki <sabyatakarki05@gmail.com>
2025-02-13 20:58:51 +0545

    updated Fragment

--- app/src/main/java/com/example/aura_app/ui/fragment/ExploreFragment.kt
+++ app/src/main/java/com/example/aura_app/ui/fragment/ExploreFragment.kt
@@ -24,7 +24,7 @@ class ExploreFragment : Fragment() {
         "Thailand known for tropical beaches,royal palaces, ancient temples displaying figures of Buddha. ",
         "Italy, a European country with a long Mediterranean coastline, has left a powerful mark on Western culture and cuisine. Its capital, Rome, is home to the Vatican as well as landmark art and ancient ruins.",
         "France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower.",
-        "The U.S. is a country of 50 states covering a vast swath of North America, with Alaska in the northwest and Hawaii extending the nation’s presence into the Pacific Ocean. Major Atlantic Coast cities are New York, a global finance and culture center, and capital Washington, DC"
+        "U.S. is a country of 50 states covering a vast swath of North America, with Alaska in the northwest and Hawaii extending the nation’s presence into the Pacific Ocean. Major Atlantic Coast cities are New York, a global finance and culture center, and capital Washington, DC"
     )
     private val ratingList = arrayListOf("4.9R", "4.7R", "4.5R","4.7R","4.2R","4.9R","4.9R")
     private val costList = arrayListOf("$500", "$1000", " $750", "$950","$550","$1000","$1500")
